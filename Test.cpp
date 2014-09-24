238552850067dbd1c2fe20661850d20cb7601cb3
milossosic <milos.m.sosic@gmail.com>
2014-09-24 19:37:53 +0200

    тњеак

--- Test.cpp
+++ Test.cpp
@@ -40,14 +40,6 @@ void Test::runVNS(char * argv, int iterations)
 	conf.fIn = string(argv);
 	
 	Writer writer(conf);
-	
-
-	//conf.fIn = "instances/14.rat99.xml";
-	//conf.fIn = "instances/09.st70.xml";
-	//conf.fIn = "instances/18.kroD100.xml";
-
-	//conf.fIn = "instances/random\/TRP-S200-R01.xml";
-	//conf.fIn = "instances/random\/TRP-S010-R01.xml";
 
 	cout << conf.fIn << endl;
 
@@ -59,57 +51,7 @@ void Test::runVNS(char * argv, int iterations)
 	vector<double> times;
 	
 
-	// 0  1000   300    10  0.91 10254 10586 1.870399976
-	// 0   400   300    20  0.97 10297 10760.4 1.108599973
 	
-
-	//for (int iter = 0; iter < 1;iter+=2)
-	//{
-	//	for (int iterDiv = 2500; iterDiv >= 1000;iterDiv-=300)
-	//	{
-	//		for (int maxTemp = 500; maxTemp <= 1500;maxTemp+=100)
-	//		{
-	//			if (iterDiv <= maxTemp)
-	//				continue;
-	//			for (int minTemp = 20; minTemp < 150;minTemp+=20)
-	//			{
-	//				for (double cool = 0.8; cool <= .92;cool+=0.02)
-	//				{
-	//					for (int i = 0; i < iterations; i++)
-	//					{
-	//						unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-	//						std::default_random_engine gen(seed);
-	//						std::uniform_int_distribution<int> dist(0, 3123121);
-	//						std::uniform_real_distribution<double> distReal(0.0, 1.0);
-	//						bool randConst = false, rvnd = false;
-	//						bool firstImprovement = false;
-	//						VNS vns(inst, gen, dist, distReal);
-	//						sa.init(maxTemp, cool, minTemp, iter, iterDiv);
-	//						time_t t = clock();
-	//						vns.VNS_SA(sa);
-	//						float time1 = ((float)(clock() - t)) / CLOCKS_PER_SEC;
-	//						costs.push_back(vns.sol.cost);
-	//						times.push_back(time1);
-	//						cout << setprecision(10) << vns.sol.cost << " " << setprecision(10) << time1 << endl;
-	//					}
-	//					calcAll(iterations, costs, times);						
-	//					//if (minCost < 10700)
-	//					//{
-	//						writer.out << setw(5) << iter << " ";
-	//						writer.out << setw(5) << iterDiv << " ";
-	//						writer.out << setw(5) << maxTemp << " ";
-	//						writer.out << setw(5) << minTemp << " ";
-	//						writer.out << setw(5) << cool << " ";
-	//						writer.out << setprecision(10) << minCost << " " << avgCost << " " << avgTime << endl;
-	//					//}							
-	//					times.clear();
-	//					costs.clear();
-	//				}
-	//			}
-	//			
-	//		}
-	//	}
-	//}
 	
 
 	SA sa;
@@ -158,7 +100,7 @@ void Test::runVNS(char * argv, int iterations)
 		}
 		calcAll(iterations, costs, times, optCost);
 
-		cout << setprecision(10) << minCost << " " << setprecision(10) << avgCost << " " << setprecision(10) << avgGap << " " << avgTime << endl;
+		cout << setprecision(10) << minCost << " " << setprecision(10) << avgCost << " " << setprecision(5) << avgGap << " " << avgTime << endl;
 		
 		writer.out << setprecision(10) << minCost << " " << setprecision(10) << avgCost << " " << setprecision(5) << avgGap << " " << avgTime << endl;
 
--- mlp_test_RELEASE.bat
+++ mlp_test_RELEASE.bat
@@ -1,6 +1,6 @@
 @echo off &setlocal enabledelayedexpansion
 @echo off
-for %%i in (.\instances\*.xml) do for /L %%A in (1,1,1) do call "D:\Milos\faks\master\MLP2\Release\MLP.exe" %%i
+for %%i in (.\instances\*.xml) do for /L %%A in (1,1,1) do call ".\Release\MLP.exe" %%i
 
 pause
 
